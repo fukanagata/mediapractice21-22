6109b908d764e092d5e591dbbabd3123287f8cea
永田風薫 <fukanagatamce@gmail.com>
2021-12-25 21:27:56 +0900

    修正

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('DOMContentLoaded', function() {
+  window.addEventListener('resize', function() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+});
+
+
+
 
 $(function() {
+
+
+
+
 
   $('.ac-parent').on('click', function() {
     $(this).next().slideToggle();
@@ -25,64 +41,73 @@ $(function() {
 
   $("#header-statement").click(function() {
     $("html,body").animate({
-      scrollTop: $('.statement-wrapper').offset().top-100
+      scrollTop: $('.statement-wrapper').offset().top - 100
     });
   });
 
   $("#header-about").click(function() {
     $("html,body").animate({
-      scrollTop: $('.about-wrapper').offset().top - $('header').height()-150
+      scrollTop: $('.about-wrapper').offset().top - $('header').height() - 150
     });
   });
   $("#header-exhibitors").click(function() {
     $("html,body").animate({
-      scrollTop: $('.exhibitors-wrapper').offset().top - $('header').height()-150
+      scrollTop: $('.exhibitors-wrapper').offset().top - $('header').height() - 150
     });
   });
   $("#header-event").click(function() {
     $("html,body").animate({
-      scrollTop: $('.event-wrapper').offset().top - $('header').height()-150
+      scrollTop: $('.event-wrapper').offset().top - $('header').height() - 150
     });
   });
   $("#header-access").click(function() {
     $("html,body").animate({
-      scrollTop: $('.access-wrapper').offset().top - $('header').height()-150
+      scrollTop: $('.access-wrapper').offset().top - $('header').height() - 150
     });
   });
 
 
-	$(function () {
-  var $header = $("#header");
-  var scrollSize = $('.header-wrapper').width()/3; //超えると表示
-  $(window).on("load scroll", function () {
-    var value = $(this).scrollTop();
-    if (value > scrollSize) {
-      $header.addClass("display");
-			$header.removeClass("hide");
-    } else {
-			$header.removeClass("display");
-			$header.addClass("hide");
-    }
+  $(function() {
+    var $header = $("#header");
+    var scrollSize = $('.header-wrapper').width() / 3; //超えると表示
+    $(window).on("load scroll", function() {
+      var value = $(this).scrollTop();
+      if (value > scrollSize) {
+        $header.addClass("display");
+        $header.removeClass("hide");
+      } else {
+        $header.removeClass("display");
+        $header.addClass("hide");
+      }
+    });
   });
-});
 
-// jQuery(function($) {
-//     $(window).scroll(function() {
-//         var offset = $(this).scrollTop()/$('body').width()*200*-1;
-//         $('.background').css( 'background-position', '0px ' + offset + 'px' );
-//     });
-//
-// 		$(window).scroll(function (){
-// 		$('.section-fadein').each(function(){
-// 			var elemPos = $(this).offset().top;
-// 			var scroll = $(window).scrollTop();
-// 			var windowHeight = $(window).height();
-// 			if (scroll > elemPos - windowHeight + 200){
-// 				$(this).addClass('scrollin');
-// 			}
-// 		});
-// 	});
-// });
+  jQuery(function($) {
+      // $(window).scroll(function() {
+      //     var offset = $(this).scrollTop()/$('body').width()*200*-1;
+      //     $('.background').css( 'background-position', '0px ' + offset + 'px' );
+      // });
+
+      $(window).scroll(function() {
+          var offset = $(this).scrollTop()/$('body').width()*2000*-1;
+          $('.pc-title').css( 'left',  offset + 'px' );
+          $('.pc-info').css( 'right', offset + 'px' );
+
+          // $('.smartphone-title').css( 'right',  offset + 'px' );
+          // $('.smartphone-info').css( 'left', offset + 'px' );
+      });
+
+  		$(window).scroll(function (){
+  		$('.section-fadein').each(function(){
+  			var elemPos = $(this).offset().top;
+  			var scroll = $(window).scrollTop();
+  			var windowHeight = $(window).height();
+  			if (scroll > elemPos - windowHeight + 200){
+  				$(this).addClass('scrollin');
+  			}
+  		});
+  	});
+  });
 
 
 
